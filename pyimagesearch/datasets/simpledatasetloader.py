150c5abf256515ae164d37c72ce4512e42dd7541
异尘 <ethan@yichendeMacBook-Pro-2.local>
2019-03-28 15:17:18 +0800

    add finetune_flowers17

--- pyimagesearch/datasets/simpledatasetloader.py
+++ pyimagesearch/datasets/simpledatasetloader.py
@@ -16,17 +16,18 @@ class SimpleDatasetLoader:
         labels = []
 
         for (i, imagePath) in enumerate(imagePaths):
-            image = cv2.imread(imagePath)
-            label = imagePath.split(os.path.sep)[-2]
-
-            if self.preprocessors is not None:
-                for p in self.preprocessors:
-                    image = p.preprocess(image)
-            
-            data.append(image)
-            labels.append(label)
-
-            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
-                print("[INFO] processed {}/{}".format(i + 1, len(imagePaths))) 
+            if '._' not in imagePath:
+                image = cv2.imread(imagePath)
+                label = imagePath.split(os.path.sep)[-2]
+    
+                if self.preprocessors is not None:
+                    for p in self.preprocessors:
+                        image = p.preprocess(image)
+                
+                data.append(image)
+                labels.append(label)
+    
+                if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                    print("[INFO] processed {}/{}".format(i + 1, len(imagePaths))) 
 
         return (np.array(data), np.array(labels))
