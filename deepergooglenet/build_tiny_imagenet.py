c7aa84cd4685ef9863ea13f39a51eff4f41ffbb5
异尘 <ethan@yichendeMacBook-Pro-2.local>
2019-04-01 16:36:18 +0800

    add build_tiny_imagenet

--- deepergooglenet/build_tiny_imagenet.py
+++ deepergooglenet/build_tiny_imagenet.py
@@ -38,3 +38,40 @@ M = [r.split('\t')[:2] for r in M]
 valPaths = [os.path.sep.join([config.VAL_IMAGES, m[0]]) for m in M]
 valLabels = le.transform([m[1] for m in M])
 
+datasets = [
+    ("train", trainPaths, trainLabels, config.TRAIN_HDF5),
+    ("val", valPaths, valLabels, config.VAL_HDF5),
+    ("test", testPaths, testLabels, config.TEST_HDF5)
+]
+
+(R, G, B) = ([], [], [])
+
+for (dType, paths, labels, outputPath) in datasets:
+    print("[INFO] building {} ...".format(outputPath))
+    writer = HDF5DatasetWriter((len(paths), 64, 64, 3), outputPath)
+
+    widgets = ["Building Dataset: ", progressbar.Percentage(), " ",
+        progressbar.Bar(), " ", progressbar.ETA()]
+    pbar = progressbar.ProgressBar(maxval=len(paths),
+        widgets=widgets).start()
+
+    for (i, (path, label)) in enumerate(zip(paths, labels)):
+        image = cv2.imread(path)
+
+        if dType == "train":
+            (b, g, r) = cv2.mean(image)[:3]
+            R.append(r)
+            G.append(g)
+            B.append(b)
+
+         writer.add([image], [label]) 
+         pbar.update(i)
+    pbar.finish()
+    writer.close()  
+
+print("[INFO] serializing means ...")
+D = {"R": np.mean(R), "G": np.mean(G), "B": np.mean(B)}
+f = open(config.DATASET_MEAN, "w")
+f.write(json.dumps(D))
+f.close()
+
